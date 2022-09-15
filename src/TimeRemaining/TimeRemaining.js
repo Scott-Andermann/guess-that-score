35accc495f807bc14c64f4e4d647f173f95582e6
Scott A <scottandermann@gmail.com>
2022-09-15 12:01:59 -0400

    add time remaining

--- src/TimeRemaining/TimeRemaining.js
+++ src/TimeRemaining/TimeRemaining.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TimeRemaining = ({gameData}) => {
     return ( 
     <div>
-        <h3>Final</h3>
+        <h3>{gameData.period}{gameData.clock ? ` - ${gameData.clock}` : ''}</h3>
         {/* {gameData.Period.includes('F') ? <h3>Final</h3> : <h3>{gameData.Period} - {gameData.TimeRemainingMinutes}:{gameData.TimeRemainingSeconds}</h3>} */}
     </div> 
     );
